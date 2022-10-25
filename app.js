e83a5c28e74f6d1d51c25cec8e614f6109e2c4ed
AnHsiL <115556520+AnHsiL@users.noreply.github.com>
2022-10-25 23:12:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var memberRouter = require('./routes/member');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,7 +19,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', memberRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -31,20 +39,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-// get values from form to enroll
-app.get('/', function(req, res){ 
-
-  var account  = req.query.account     //帳號
-  var password = req.query.password    //密碼
-  var email    = req.query.email       //email
-
-  //send value to router
-  res.send({  
-    account:  account,
-    password: password,
-    email:    email
-  });
-
-  res.end()
-});

26a75167abff654fd5aac8bbb221ab8dae170fdd
LAH <00957047@email.ntou.edu.tw>
2022-11-18 00:47:23 +0800

    add: passenger model

--- models/passengerInfo_model.js
+++ models/passengerInfo_model.js
@@ -2,6 +2,7 @@ var MongoClient = require('mongodb').MongoClient;
 
 module.exports = function inputPassengerData(passengerData){
 
+    let result = {};
     return new Promise((resolve, reject) =>{
         MongoClient.connect("mongodb://localhost:27017/signData", function(err,db){
             if(err){
@@ -13,18 +14,8 @@ module.exports = function inputPassengerData(passengerData){
             }
 
             var dbo = db.db('signData');
-            var myQuery = {account:passengerData.account};
             
-            dbo.collection('userList').updateOne(myQuery, passengerData, function(err, res){
-                if(err){
-                    console.log("資料庫連線失敗");
-                    result.status = "連線失敗"
-                    result.err = "伺服器錯誤!"
-                    reject(result);
-                    return;
-                }
-                console.log("updated successfully");
-            });
+            dbo.collection('userList').updateOne({account:passengerData.account}, {$set:passengerData}, {upsert:true});
             result.passengerMember = passengerData;
             resolve(result);
         })

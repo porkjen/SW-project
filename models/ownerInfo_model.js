6225ac056ac66bb7e7b55ef28ddbcbda75d4cc60
LAH <00957047@email.ntou.edu.tw>
2022-11-17 20:27:50 +0800

    add: owner status

--- models/ownerInfo_model.js
+++ models/ownerInfo_model.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 
-module.exports = function inputPassengerData(ownerData){
+module.exports = function inputOwnerData(ownerData){
 
     return new Promise((resolve, reject) =>{
         MongoClient.connect("mongodb://localhost:27017/signData", function(err,db){

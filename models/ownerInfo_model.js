488a600abf535abe63f3a94c21f9485f21689052
LAH <00957047@email.ntou.edu.tw>
2022-11-17 23:48:08 +0800

    add:input owner data

--- models/ownerInfo_model.js
+++ models/ownerInfo_model.js
@@ -2,6 +2,7 @@ var MongoClient = require('mongodb').MongoClient;
 
 module.exports = function inputOwnerData(ownerData){
 
+    let result = {};
     return new Promise((resolve, reject) =>{
         MongoClient.connect("mongodb://localhost:27017/signData", function(err,db){
             if(err){
@@ -14,16 +15,7 @@ module.exports = function inputOwnerData(ownerData){
 
             var dbo = db.db('signData');
             
-            dbo.collection('userList').findByIdAndUpdate(ownerData.account, ownerData, function(err, res){
-                if(err){
-                    console.log("資料庫連線失敗");
-                    result.status = "連線失敗"
-                    result.err = "伺服器錯誤!"
-                    reject(result);
-                    return;
-                }
-                console.log("更新成功");
-            });
+            dbo.collection('userList').updateOne({account:ownerData.account}, {$set:ownerData}, {upsert:true});
             result.ownerMember = ownerData;
             resolve(result);
         })

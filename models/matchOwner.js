5d2e79095e874aa20401194b5a93e569a2c4c591
AnHsiL <115556520+AnHsiL@users.noreply.github.com>
2022-11-22 22:18:51 +0800

    Update matchOwner.js

--- models/matchOwner.js
+++ models/matchOwner.js
@@ -1,24 +1,26 @@
 var MongoClient = require('mongodb').MongoClient;
 
 //find and list avaliable online driver from mongoDB, sort by having a helmet
-module.exports = function matchOwner(ownerData){
+module.exports = async function matchOwner(matchData){
 
     let result = {};
     
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url,  {useNewUrlParser: true}, function(err, db) {
       if (err) throw err;
       var dbo = db.db("mydb");
-      dbo.collection("userList").find({identity:"owner", status:"online"},
-      {projection: {account:0, name:1, phone:1, gender:1, licensePlateNum:1, workingTime:0, helmet:1, other:1, status:1, identity:0} }).toArray(function(err, result) {
+      dbo.collection("userList").find({ identity: "owner", status: "online", location: matchData.area },
+        { projection: { account: 0, name: 1, phone: 1, email: 0, gender: 1, licensePlateNum: 1, 
+          location: 1, workingTime: 1, helmet: 1, other: 1, status: 1, identity: 0 } }).toArray(function (err, result) {
 
-        if (err) throw err;
-        result.sort(function(a,b){
-          return a.helmet > b.helmet;
+          if (err)
+            throw err;
+          result.sort(function (a, b) {
+            return a.helmet > b.helmet;
+          });
+
+          console.log(result);
+          db.close();
         });
-
-        console.log(result);
-        db.close();
-      });
     });
 
     return result;

30f938f694a794d52e87d7fe4b71a7463019a266
SY <86911128+everything0216@users.noreply.github.com>
2022-11-29 20:06:22 +0800

    Add files via upload

--- public/javascripts/mainPage_scripts.js
+++ public/javascripts/mainPage_scripts.js
@@ -1,48 +1,42 @@
-
-function btn1(zone){
-    $(".get_info").empty();
+function btn1(){
+    //$(".card-group").empty();
     $.ajax({
-        url: '',
+        url: 'http://127.0.0.1:3000/all',
         type: 'get',
         success: function(data){
-          for(i = 0;i<5;i++){
-              if(data[i]){
-                    $('.get_info').append(
-                        '<table>'+
-                        '<tr>'
-
+            for(i = 0;i<5;i++){
+                console.log(data.data[i].status);
+              if(data.data[i]){
+                console.log(i);
+                    $('.container').append(
+                        '<div class="card">'+
+                                '<div class="card-body">'+
+                                '<h5 class="card-title">'+data.data[i].name+'</h5>'+
+                                '<h6 class="card-subtitle mb-2 text-muted">'+data.data[i].gender+'</h6>'+
+                                '<p class="card-text">'+'搭乘地點:'+'<br>'+'目的地:'+'<br>'+'安全帽:'+'<br>'+'其他資訊:'+'<br>'+'sfsfsfsnuinuisnvnsnsndnsiudnfunsfndfkjsdfjnsnsuie'+'</p>'+
+                                '<button type="button" class="btn btn-outline-success" id="'+data.data[i].email+'" onclick="sendMail(this.id)">接受</button>'+
+                                '<button type="button" class="btn btn-outline-success" id="reject" onclick="removeCard()">拒絕</button>'+
+                              '</div>'+
+                        '</div>'
                     )
               }
-
-          }
+            }
         },
         error: function(){
-          //alert("wrong");
+          alert("wrong");
         }
       })
 }
 
 function removeCard(){
-    var eDiv = document.getElementById("info_1");
+    var eDiv = document.getElementById("container");
     eDiv.parentNode.removeChild(eDiv);
 }
+
+
+
 
 $('.close').click(function(){
     var $target = $(this).parent('div');
     $target.hide('slow',function(){$target.remove();})
 })
-
-/*
-fetch('http://127.0.0.1:8080/SightAPI?zone=中山',{mode:"cors"})
-    .then((response) => {
-
-        return response.json();
-    })
-    .then( (response) => {
-        console.log(response);
-    })
-    .catch((error) => {
-        //console.log(`Error: ${error}`);
-    })
-
-*/

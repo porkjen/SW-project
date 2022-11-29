8d4b9cc481652aa38f876cbc85415558c0d669ff
SY <86911128+everything0216@users.noreply.github.com>
2022-11-30 00:29:40 +0800

    Add files via upload

--- public/javascripts/mainPage_scripts.js
+++ public/javascripts/mainPage_scripts.js
@@ -5,17 +5,16 @@ function btn1(){
         type: 'get',
         success: function(data){
             for(i = 0;i<5;i++){
-                console.log(data.result[i].status);
-              if(data.result[i]){
-                console.log(data.result[i]);
+              if(data.data[i]){
+                console.log(i);
                     $('.container').append(
-                        '<div class="card">'+
+                        '<div class="card" id="'+i+'">'+
                                 '<div class="card-body">'+
-                                '<h5 class="card-title">'+data.result[i].name+'</h5>'+
-                                '<h6 class="card-subtitle mb-2 text-muted">'+data.result[i].gender+'</h6>'+
-                                '<p class="card-text">'+'搭乘地點:'+'<br>'+'目的地:'+'<br>'+'安全帽:'+'<br>'+'其他資訊:'+'<br>'+'sfsfsfsnuinuisnvnsnsndnsiudnfunsfndfkjsdfjnsnsuie'+'</p>'+
-                                '<button type="button" class="btn btn-outline-success" id="'+data.result[i].email+'" onclick="sendMail(this.id)">接受</button>'+
-                                '<button type="button" class="btn btn-outline-success" id="reject" onclick="removeCard()">拒絕</button>'+
+                                '<h5 class="card-title">'+data.data[i].name+'</h5>'+
+                                '<h6 class="card-subtitle mb-2 text-muted">'+data.data[i].gender+'</h6>'+
+                                '<p class="card-text">'+'搭乘地點:'+'<br>'+'目的地:'+'<br>'+'安全帽:'+data.data[i].helmet+'<br>'+'其他資訊:'+'<br>'+data.data[i].other+'</p>'+
+                                '<button type="button" class="btn btn-outline-success" id="accept" onclick="getInfo('+i+')">接受</button>'+
+                                '<button type="button" class="btn btn-outline-success" id="reject" onclick="removeCard('+i+')">拒絕</button>'+
                               '</div>'+
                         '</div>'
                     )
@@ -28,15 +27,72 @@ function btn1(){
       })
 }
 
-function removeCard(){
-    var eDiv = document.getElementById("container");
+function removeCard(num){
+    var eDiv = document.getElementById(num);
     eDiv.parentNode.removeChild(eDiv);
+}
+
+function getInfo(num){
+    $.ajax({
+        url: 'http://127.0.0.1:3000/listPassenger',
+        type: 'get',
+        success: function(data){
+           info = JSON.stringify(data.result[num]);
+           sendMail(info);
+        },
+        error: function(){
+          alert("wrong");
+        }
+      })
 }
 
 
 
+function sendMail(n) {
+    fetch('', {
+          body: n, // must match 'Content-Type' header
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached         
+          headers: {
+            'content-type': 'application/json'
+          },
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors'
+        })
+        .then(response => console.log(n))
+        .then(response => response.json()) 
+        .catch(function() {
+          console.log(n);
+        });
+
+}
 
 $('.close').click(function(){
     var $target = $(this).parent('div');
     $target.hide('slow',function(){$target.remove();})
 })
+
+/*
+fetch('http://127.0.0.1:8080/SightAPI?zone=中山',{mode:"cors"})
+    .then((response) => {
+
+        return response.json();
+    })
+    .then( (response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        //console.log(`Error: ${error}`);
+    })
+
+*/
+/*
+<div class="card" id="info_1">
+                    <div class="card-body">
+                      <h5 class="card-title">林欣儀</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">女</h6>
+                      <p class="card-text">搭乘地點:<br>目的地:<br>安全帽:<br>其他資訊:<br>sfsfsfsnuinuisnvnsnsndnsiudnfunsfndfkjsdfjnsnsuie</p>
+                      <button type="button" class="btn btn-outline-success" id="accept" onclick="sendMail()">接受</button>
+                      <button type="button" class="btn btn-outline-success" id="reject" onclick="removeCard()">拒絕</button>
+                    </div>
+                </div>
+*/

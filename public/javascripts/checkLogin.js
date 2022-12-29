474ba4c052e22c03a134b04e31165737ba94ec8d
LAH <00957047@email.ntou.edu.tw>
2022-12-29 22:57:31 +0800

    add: 美化 alert 成 swal

--- public/javascripts/checkLogin.js
+++ public/javascripts/checkLogin.js
@@ -11,12 +11,20 @@ function checkLogin(user_account, user_password){
         data: {account : user_account, password: user_password},
         success: function(chkRes){
             if(chkRes.result == "no user"){
-                alert("帳號或密碼錯誤! 請重新輸入!");
-                location.reload();
+                swal({
+                    title: "帳號或密碼錯誤! 請重新輸入!",
+                    icon: "error",
+                }).then(function () {
+                    location.reload();
+                });
             }
             else if(chkRes.result == "find user"){
-                alert("登入成功 !");
-                location.href="/chooseIdentity.html";
+                swal({
+                    title: "登入成功 !",
+                    icon: "success",
+                }).then(function () {
+                    location.href="/chooseIdentity.html";
+                });
             }
             else{
                 alert("login error.");
